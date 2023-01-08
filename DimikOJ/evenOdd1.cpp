0905cd5b442b2e20b34ca29cbfebbdadfd048214
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2023-01-08 21:08:53 +0600

    Create evenOdd1.cpp

---
+++ DimikOJ/evenOdd1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+    #endif
+
+        int t;
+        cin>>t;
+
+        int num;
+        for (int i = 0; i < t; ++i)
+        {
+        	cin>>num;
+
+        if (num%2==0)
+        {
+        	cout<<"even"<<endl;
+        }
+        else cout<<"odd"<<endl;
+    }
+	return 0;
+}
